52430ea7f72725c6c8bcbc1410d04eeba4cb2b9f
Andrew Castano <stainsteelcrown@gmail.com>
2013-11-21 14:34:46 -0600

    Form data ends up in the URL unless form method post

--- castanohomework1.py
+++ castanohomework1.py
@@ -8,10 +8,11 @@ import cgi
 def escape_html(s):
     return cgi.escape(s, quote = True)
 
-textarea="""
-<form> 
+form="""
+<form method="post"> 
     <h1>Enter some text to Rot13</h1>
-    <textarea style="width: 350px; height: 150px;" name="rot13" value=%(rot13)s method="post">
+    <br>
+    <textarea style="width: 350px; height: 150px;" name="rot13" value=%(rot13)s> 
     </textarea>
     <br>
     <input type="submit">
@@ -20,15 +21,14 @@ textarea="""
 
 class MainPage(webapp2.RequestHandler):
 
-    def write_textarea(self, error="", rot13=""):
-        self.response.out.write(textarea % {"error":error,
-                                            "rot13":rot13})
+    def write_form(self, error="", rot13=""):
+        self.response.out.write(form) 
+                                    
     def get(self):
-        self.write_textarea()
+        self.write_form()
 
     def post(self):
         user_input = self.request.get('rot13')
-
 
 application = webapp2.WSGIApplication([('/', MainPage),
 ], debug=True)
