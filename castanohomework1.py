c4766917ca7775e55bf9fb82d65320668851e7aa
Andrew Castano <stainsteelcrown@gmail.com>
2013-11-21 14:12:56 -0600

    Learning about the post handler

--- castanohomework1.py
+++ castanohomework1.py
@@ -1,13 +1,34 @@
-import webapp2
+#!/usr/bin/env python
+# Andrew Castano
+# Casenco Inc.
 
+import webapp2
+import cgi
+
+def escape_html(s):
+    return cgi.escape(s, quote = True)
+
+textarea="""
+<form> 
+    <h1>Enter some text to Rot13</h1>
+    <textarea style="width: 350px; height: 150px;" name="rot13" value=%(rot13)s method="post">
+    </textarea>
+    <br>
+    <input type="submit">
+</form> 
+"""
 
 class MainPage(webapp2.RequestHandler):
 
-        def get(self):
-                    self.response.headers['Content-Type'] = 'text/plain'
-                            self.response.write('Hello, World!')
+    def write_textarea(self, error="", rot13=""):
+        self.response.out.write(textarea % {"error":error,
+                                            "rot13":rot13})
+    def get(self):
+        self.write_textarea()
+
+    def post(self):
+        user_input = self.request.get('rot13')
 
 
-                            application = webapp2.WSGIApplication([
-                                    ('/', MainPage),
-                                    ], debug=True)
+application = webapp2.WSGIApplication([('/', MainPage),
+], debug=True)
