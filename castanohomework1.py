7f8d27961623e7f618594a755b23f437c5099cb2
Andrew Castano <stainsteelcrown@gmail.com>
2013-11-21 17:16:01 -0600

    Before checking answer

--- castanohomework1.py
+++ castanohomework1.py
@@ -4,6 +4,7 @@
 
 import webapp2
 import cgi
+import string
 
 def escape_html(s):
     return cgi.escape(s, quote = True)
@@ -12,27 +13,43 @@ form="""
 <form method="post"> 
     <label>
     <h1>Enter some text to Rot13</h1>
-    <br>
-    <textarea name="text" rows="10" cols="80">
-        %(text)s
-    </textarea>
+    <textarea name="text" rows="10" cols="80">%(text)s</textarea>
     </label>
     <br>
     <input type="submit">
 </form> 
 """
 
+def rot13(user_input):
+    upper = string.uppercase
+    lower = string.lowercase
+    alpha_str = ""
+    for i in user_input:
+        if i.isalpha():
+            if i.isupper():
+                l = upper.index(i)
+                i = upper[(l+13)%len(upper)]
+            else:
+                l = lower.index(i)
+                i = lower[(l+13)%len(upper)]
+            
+            alpha_str = alpha_str + i
+        else:
+            alpha_str = alpha_str + i
+    return alpha_str
+
 class MainPage(webapp2.RequestHandler):
 
-    def write_form(self, text="") 
-        self.response.out.write(form % {"text": text}) 
+    def write_form(self, text=""): 
+        self.response.out.write(form % {"text": escape_html(text)}) 
                                     
     def get(self):
-        self.write_form("Begin Here")
+        self.write_form()
 
     def post(self):
         user_input = self.request.get('text') 
-        self.write_form(user_input)
+        rotty = rot13(user_input)
+        self.write_form(rotty)
 
 application = webapp2.WSGIApplication([('/', MainPage),
 ], debug=True)
